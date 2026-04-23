2bec2a323f43faca93820aed2bed260ea9df0f52
Luc Alencar <lucalencar.dev@gmail.com>
2026-04-23 14:11:42 +0000

    Updates

--- app1.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t,R as o}from"./client.js";import{A as r}from"./App.js";e.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsx(r,{})}));
--- app2.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,R as o}from"./client.js";import{H as r}from"./App22.js";t.createRoot(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{})}));
